fd34bc7946f9c730859c68fa136c31fa94569761
Jim Kasper <jimkasper@Jims-MacBook-Pro.local>
2015-09-29 16:12:14 -0400

    first commit

--- projecteuler5.java
+++ projecteuler5.java
@@ -1,8 +1,56 @@
-public class MetersToFeet {
+public class projecteuler5 {
 	public static void main(String[] args) {
 		
+		float m = 1 ;
+		for(int x = 1; x <= 20; x++){
+			m = m*x	;
+		}
+			
+		System.out.println(m);
 		
-System.out.println(m);	
+		for(int z = 20; z < m ; z++){
+		//	System.out.println(z);
+			/*
+		for(int s = 1; s%; s++)
+			
+			System.out.println(s);
+	}
+		*/
+				
+		if (z % 2 == 0)
+				if (z % 3 == 0)
+				if (z % 4 == 0)
+				if (z % 5 == 0)
+						
+				if (z % 6 == 0)
+				if (z % 7 == 0)
+				if (z % 8 == 0)
+				if (z % 9 == 0)
+				if (z % 10 == 0)
+						
+				if (z % 11 == 0)
+				if (z % 12 == 0)
+				if (z % 13 == 0)
+				if (z % 14 == 0)
+						
+						if (z % 15 == 0)
+						if (z % 16 == 0)
+						if (z % 17 == 0)
+						if (z % 18 == 0)
+								
+						if (z % 19 == 0)
+						if (z % 20 == 0)
+								
+					System.out.println(z);
+			
+		
+		
+			
+		//	if(m % 10 == 0) {
+			//	System.out.println("");
+		
+	
 
+		}
 	}
 }
