0acb0fcd55589a265ac36548207f3ae6f6b91fe7
Daniel Wintrich <danielwintrich@Daniels-MacBook-Pro.local>
2015-09-29 17:27:59 -0400

    Condensed if statements into for loops.

--- projecteuler5.java
+++ projecteuler5.java
@@ -1,32 +1,15 @@
 public class projecteuler5 {
 	public static void main(String[] args) {
 		
-		float m = 1 ;
-		for(int x = 1; x <= 20; x++){
-			m = m*x	;
-		}
-
-		for(int z = 20; z < 2147483647 ; z++){	
-		if (z % 2 == 0)
-			if (z % 3 == 0)
-				if (z % 4 == 0)
-					if (z % 5 == 0)
-						if (z % 6 == 0)
-							if (z % 7 == 0)
-								if (z % 8 == 0)
-									if (z % 9 == 0)
-										if (z % 10 == 0)
-											if (z % 11 == 0)
-												if (z % 12 == 0)
-													if (z % 13 == 0)
-														if (z % 14 == 0)
-															if (z % 15 == 0)
-																if (z % 16 == 0)
-																	if (z % 17 == 0)
-																		if (z % 18 == 0)
-																			if (z % 19 == 0)
-																				if (z % 20 == 0)
-																					System.out.println(z);
+		for(int z = 1; z < 2147483647; z++) {
+			boolean valid = true;
+			for (int i = 2; i <= 20; i++) {
+				if (valid == true)
+					if (z % i != 0)
+						valid = false;
+			}
+			if (valid == true)
+				System.out.println(z);
 		}
 	}
 }
