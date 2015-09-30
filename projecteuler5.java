56f4e6069ac9cbd9f71b0789df2c3dee3a67df53
James Kasper <jkasper311@gmail.com>
2015-09-30 10:23:41 -0400

    optimized

--- projecteuler5.java
+++ projecteuler5.java
@@ -1,15 +1,30 @@
 public class projecteuler5 {
 	public static void main(String[] args) {
 		
-		for(int z = 1; z < 2147483647; z++) {
-			boolean valid = true;
-			for (int i = 2; i <= 20; i++) {
-				if (valid == true)
-					if (z % i != 0)
-						valid = false;
+		int answer = 0;
+		/* 
+			optimization: initialize x to 20 and increment by 20
+						this has the effect of only iteratiing multiples of 20
+		*/
+		
+		for(int x = 20; answer == 0; x = x+20) {
+			boolean isDivisibleByAll = true;
+			/* 
+				optimization: initialize i to 10
+							this can be done because any number that is a multiple of 11-20
+				will also be a multiple of 1-10
+			*/
+			for (int i = 11; i <= 20; i++) {
+					if (x % i != 0){
+						isDivisibleByAll = false;
+				}
 			}
-			if (valid == true)
-				System.out.println(z);
+			if (isDivisibleByAll){
+				answer = x;
+				
+				
+			}
 		}
+		System.out.println(answer);
 	}
 }
